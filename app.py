841a4c81226958da4427978d97a07530340fabc2
Piotr Ciastko <ciaasteczkowy@gmail.com>
2016-09-10 11:55:02 +0200

    yay

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+import piecash
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	try:
+		book = piecash.open_book(uri_conn=os.environ['JAWSDB_URL'], readonly=False, backup=False)
+	except Exception as e:
+		return e.message
+	return 'Hello World!'
