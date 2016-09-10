b43c294cad1bdaea31b2c1e4db0bf371704ce1a1
Piotr Ciastko <ciaasteczkowy@gmail.com>
2016-09-10 14:33:08 +0200

    meh

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
 import piecash
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	try:
-		book = piecash.open_book(uri_conn=os.environ['JAWSDB_URL'], readonly=False)
-	except Exception as e:
-		return e.message
-	return 'Hello World!'
+	book = piecash.open_book(uri_conn=os.environ['JAWSDB_URL'], readonly=False, do_backup=False)
+	return render_template("index.html", accounts=book.accounts)
+
+
+app.run(port=8000, debug=True)
