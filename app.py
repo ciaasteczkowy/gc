d0cdab9806cbeea53adf7fc167676f5f1f41b07f
Piotr Ciastko <ciaasteczkowy@gmail.com>
2016-09-10 11:58:32 +0200

    yay

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 	try:
-		book = piecash.open_book(uri_conn=os.environ['JAWSDB_URL'], readonly=False, backup=False)
+		book = piecash.open_book(uri_conn=os.environ['JAWSDB_URL'], readonly=False)
 	except Exception as e:
 		return e.message
 	return 'Hello World!'
