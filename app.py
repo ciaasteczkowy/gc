8ed61c8b1eb73d6ba38adb210cce6e1ac99e0b3d
Piotr Ciastko <ciaasteczkowy@gmail.com>
2016-09-10 14:53:45 +0200

    meh

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ import piecash
 from flask import Flask, render_template
 import json
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	book = piecash.open_book(uri_conn=os.environ['JAWSDB_URL'], readonly=False, do_backup=False)
-	return render_template("index.html", accounts=book.accounts)
 
 
-app.run(port=8000, debug=True)
+
+	return render_template("index.html", accounts=book.root_account.children(name="Wydatki").children)
+
+
+app.run(host="0.0.0.0", port=8000, debug=True)
