5ce9555c4238be34870b88872893f240e42665f5
Piotr Ciastko <ciaasteczkowy@gmail.com>
2016-09-10 14:56:14 +0200

    meh

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def hello():
 	return render_template("index.html", accounts=book.root_account.children(name="Wydatki").children)
 
 
-app.run(host="0.0.0.0", port=8000, debug=True)
+# app.run(host="0.0.0.0", port=8000, debug=True)
