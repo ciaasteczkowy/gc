089bb337a381598ff137695c23d651ac48b89e30
Piotr Ciastko <ciaasteczkowy@gmail.com>
2016-09-10 18:05:37 +0200

    meh

--- app.py
+++ app.py
@@ -12,9 +12,32 @@ app = Flask(__name__)
 def hello():
 	book = piecash.open_book(uri_conn=os.environ['JAWSDB_URL'], readonly=False, do_backup=False)
 
+	income = get_income(book)
+	outcome = get_expense(book)
+
+	return render_template("index.html", income=income, outcome=outcome, balance=income-outcome)
+
+def get_splits_sum(account):
+	_sum = 0
+	for split in account.splits:
+		_sum += split.value
+	return _sum
+
+def get_income(book):
+	income = 0
+	for account in book.accounts:
+		if account.type == "INCOME" and account.placeholder != 1:
+			income -= get_splits_sum(account)
+
+	return income
+
+def get_expense(book):
+	expense = 0
+	for account in book.accounts:
+		if account.type == "EXPENSE" and account.placeholder != 1:
+			expense += get_splits_sum(account)
+
+	return expense
 
 
-	return render_template("index.html", accounts=book.root_account.children(name="Wydatki").children)
-
-
-# app.run(host="0.0.0.0", port=8000, debug=True)
+app.run(host="0.0.0.0", port=8000, debug=True)
