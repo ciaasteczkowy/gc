882249de96ba4036f4322ca2e24fd764afbe8bdf
Piotr Ciastko <piotr.ciastko@siroccomobile.com>
2016-09-12 08:45:15 +0200

    bootstrap

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
 import piecash
-from flask import Flask, render_template
-import json
-
 import pymysql
+from flask import Flask, render_template
+
 pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
@@ -13,54 +12,60 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def hello():
-	try:
-		book = piecash.open_book(uri_conn=os.environ['JAWSDB_URL'], readonly=False, do_backup=False, open_if_lock=True)
-	except Exception as e:
-		return '{}'.format(e)
+    try:
+        book = piecash.open_book(uri_conn=os.environ['JAWSDB_URL'], readonly=False, do_backup=False,
+                                 open_if_lock=True)
+    except Exception as e:
+        return '{}'.format(e)
 
-	income = get_income(book)
-	outcome = get_expense(book)
-	in_tree = income_tree(book.root_account.children(type="INCOME"))
-	ex_tree = income_tree(book.root_account.children(type="EXPENSE"))
+    income = get_income(book)
+    outcome = get_expense(book)
+    in_tree = income_tree(book.root_account.children(type="INCOME"))
+    ex_tree = income_tree(book.root_account.children(type="EXPENSE"))
 
-	return render_template("index.html", income=income, outcome=outcome, balance=income+outcome, in_tree=in_tree, ex_tree=ex_tree)
+    return render_template("index.html", income=income, outcome=outcome, balance=income + outcome,
+                           in_tree=in_tree, ex_tree=ex_tree)
+
 
 def get_splits_sum(book, account_type):
-	_sum = 0
-	for account in book.accounts:
-		if account.type == account_type and account.placeholder != 1:
-			for split in account.splits:
-				_sum -= split.value
+    _sum = 0
+    for account in book.accounts:
+        if account.type == account_type and account.placeholder != 1:
+            for split in account.splits:
+                _sum -= split.value
 
-	return _sum
+    return _sum
+
 
 def account_balance(account):
-	balance = 0
-	for split in account.splits:
-		balance -= split.value
+    balance = 0
+    for split in account.splits:
+        balance -= split.value
 
-	return balance
+    return balance
+
 
 def get_income(book):
-	return get_splits_sum(book, "INCOME")
+    return get_splits_sum(book, "INCOME")
+
 
 def get_expense(book):
-	return get_splits_sum(book, "EXPENSE")
+    return get_splits_sum(book, "EXPENSE")
+
 
 def income_tree(account):
-	tree = {
-		'name': u'{}'.format(account.name),
-		'balance': account_balance(account),
-		'children': []
-	}
-	for acc in account.children:
-		tree['children'].append(income_tree(acc))
+    tree = {
+        'name': u'{}'.format(account.name),
+        'balance': account_balance(account),
+        'children': []
+    }
+    for acc in account.children:
+        tree['children'].append(income_tree(acc))
 
-	for child in tree['children']:
-		tree['balance'] += child['balance']
+    for child in tree['children']:
+        tree['balance'] += child['balance']
 
-	return tree
+    return tree
 
 
-
-# app.run(host="0.0.0.0", port=8000, debug=True)
+app.run(host="0.0.0.0", port=8000, debug=True)
